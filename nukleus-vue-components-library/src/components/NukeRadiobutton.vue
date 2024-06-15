78d2672d0ab974d43f04346a224e7a67272d04dd
Kid A <me@thisismeamir.com>
2024-06-15 11:11:32 +0330

    [Implementation] NukeRadiobutton.vue implemented completely

--- nukleus-vue-components-library/src/components/NukeRadiobutton.vue
+++ nukleus-vue-components-library/src/components/NukeRadiobutton.vue
@@ -1,90 +1,68 @@
 <template>
-  <input type="radio" :value="label" v-model="model" class="hidden" v-bind="$attrs" :disabled="disabled" :label="label" />
-  <div
-      class="
-    max-h-[24px]
-    max-w-[24px]
-    rounded-full
-    group flex items-center justify-center text-white
-    h-[--nuke-space-primary]
-    w-[--nuke-space-primary]
+  <div class="
+  rounded-full
+  nuke-color-attribute
+  transition-colors
+  group flex items-center justify-center
+  h-[16px]
+  w-[16px]
+  data-[checked=true]:bg-[--nuke-color-primary-700]
+  data-[checked=false]:bg-[--nuke-color-secondary-300]
+  data-[checked=false]:dark:bg-[--nuke-color-secondary-900]
+  "
+       :data-checked="computedChecked"
+       :nuke-color-primary="primaryColor"
+       :nuke-color-secondary="secondaryColor"
+
+       @click="() => {
+    if(model != id){
+    computedChecked = !computedChecked
+    model = id
+    }
+  }">
+    <div class="rounded-full
+    transition-all
+    duration-200
     nuke-color-attribute
-    nuke-space-attribute
-    nuke-transition-colors
-
-    data-[disabled=false]:data-[checked=false]:bg-[--nuke-color-secondary-200]
-    data-[disabled=false]:data-[checked=false]:hover:bg-[--nuke-color-secondary-300]
-    data-[disabled=false]:data-[checked=false]:dark:bg-[--nuke-color-secondary-900]
-    data-[disabled=false]:data-[checked=false]:dark:hover:bg-[--nuke-color-secondary-800]
-
-    data-[disabled=false]:data-[checked=true]:bg-[--nuke-color-primary-700]
-    data-[disabled=false]:data-[checked=true]:hover:bg-[--nuke-color-primary-800]
-
-    data-[disabled=true]:data-[checked=false]:bg-[--nuke-color-secondary-200]
-    data-[disabled=true]:data-[checked=false]:dark:bg-[--nuke-color-secondary-800]
-    data-[disabled=true]:data-[checked=true]:bg-[--nuke-color-secondary-800]
-    "
-      :nuke-color-primary="primaryColor"
-      :nuke-color-secondary="secondaryColor"
-      :nuke-space-primary="size"
-      :data-checked="model === label"
-      @click="checkFunction()"
-      :data-disabled="disabled"
-  >
-    <div
-        class="
-      transition-opacity
-      w-[64%] h-[64%]
-      rounded-full
-      group-data-[checked=false]:opacity-0
-      group-data-[checked=true]:opacity-100
-
-      group-data-[checked=true]:dark:bg-[--nuke-color-secondary-300]
-      group-data-[checked=true]:bg-[--nuke-color-secondary-300]
-      group-data-[disabled=true]:bg-[--nuke-color-secondary-900]
-    "/>
+    group-data-[checked=false]:scale-0
+    group-data-[checked=true]:scale-100
+    h-[8px] w-[8px]
+    bg-[--gray-300]
+  "/>
   </div>
 </template>
 
 <script setup lang="ts">
-import { Color } from "../composables/useColor.ts";
-import { Space } from "../composables/useSize.ts";
-import { ref, watch, computed } from "vue";
+
+import {Color} from "../composables/useColor.ts";
+import {computed, watch} from "vue";
+
+const model = defineModel<string>()
+const emit = defineEmits(["modelValue"]);
+
+watch(model, (newValue) => {
+  emit("modelValue", newValue)
+})
 
 const props = withDefaults(
     defineProps<{
-      modelValue: string;
-      name: string;
-      label: string;
-      primaryColor: Color;
-      secondaryColor: Color;
-      size: Space;
-      disabled: boolean;
-    }>(),
-    {
-      disabled: false,
-      size: "24px",
+      name: string
+      id: string
+      value: string
+      primaryColor: Color
+      secondaryColor: Color
+    }>(), {
+      value: "",
       secondaryColor: "gray",
       primaryColor: "azure",
+      checked: false
+
     }
-);
+)
 
-const model = ref(props.modelValue);
-
-function checkFunction() {
-  if (!props.disabled && props.modelValue !== props.label) {
-      model.value = props.label;
-    emitUpdate();
-  }
-}
-
-function emitUpdate() {
-  emit('update:modelValue', model.value);
-}
-
-watch(() => props.modelValue, (newValue) => {
-  model.value = newValue;
-});
+const computedChecked = computed(()=>{
+  return model.value == props.id
+})
 </script>
 
 <style scoped lang="scss">
