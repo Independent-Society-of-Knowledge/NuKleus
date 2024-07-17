8f13115c49d14369512c6a47b74972b3e10690bd
Kid A <me@thisismeamir.com>
2024-07-17 17:52:11 +0330

    [implementation] NukCheckbox Implemented

---
+++ vue-sandbox/src/components/NukCheckbox.vue
@@ -0,0 +1,118 @@
+<!--
+  - Nukleus: A design system for knowledge (vue-components-library)
+  -
+  - Copyright © 2024 Independent Society of Knowledge
+  - This program is free software: you can redistribute it and/or modify
+  - it under the terms of the GNU General Public License as published by
+  - the Free Software Foundation, either version 2 of the License, or
+  - (at your option) any later version.
+  -
+  - This program is distributed in the hope that it will be useful,
+  - but WITHOUT ANY WARRANTY; without even the implied warranty of
+  - MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+  - GNU General Public License for more details.
+  -
+  - You should have received a copy of the GNU General Public License
+  - along with this program. If not, see <https://www.gnu.org/licenses/>.
+  -
+  - Contact Information:
+  - Independent Society of Knowledge
+  - Email: projects@iskportal.com
+  -
+  - SPDX-License-Identifier: GPL-2.0-or-later
+  -
+  - Developed by: Amir H. Ebrahimnezhad (if you helped or your commits are going to be pulled please add your name and email with a comma.)
+  - Email:        ceo@iskportal.com
+  - version 1.0.0
+  -
+  -->
+
+<template>
+  <input type="checkbox" v-model="model" class="hidden" :disabled="disabled"/>
+  <div class="
+    h-16px
+    w-16px
+    group flex items-center justify-center text-white
+
+    nuk-coloring
+    nuk-transition-background-color
+    nuk-duration-fast-02
+    nuk-standard-productive
+
+    data-[disabled=false]:data-[checked=false]:bg-[--nuk-secondary-20]
+    data-[disabled=false]:data-[checked=false]:hover:bg-[--nuk-secondary-30]
+    data-[disabled=false]:data-[checked=false]:dark:bg-[--nuk-secondary-90]
+    data-[disabled=false]:data-[checked=false]:dark:hover:bg-[--nuk-secondary-80]
+
+    data-[disabled=false]:data-[checked=true]:bg-[--nuk-primary-70]
+    data-[disabled=false]:data-[checked=true]:hover:bg-[--nuk-primary-80]
+
+    data-[disabled=true]:text-[--nuk-secondary-50]
+    data-[disabled=true]:data-[checked=false]:bg-[--nuk-secondary-20]
+    data-[disabled=true]:data-[checked=false]:dark:bg-[--nuk-secondary-90]
+    data-[disabled=true]:data-[checked=true]:bg-[--nuk-secondary-90]
+
+    "
+       :nuk-color-primary="color"
+       nuk-color-secondary="gray"
+       @click="checkFunction()"
+       :data-checked="model"
+       :data-disabled="disabled">
+
+
+    <div class="
+      nuk-transition-opacity
+      nuk-duration-fast-02
+      nuk-standard-productive
+      group-data-[checked=false]:opacity-0
+      group-data-[checked=true]:opacity-100
+    ">
+      <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" fill="none" version="1.1"
+           width="16" height="16" viewBox="0 0 20 20">
+        <defs>
+          <clipPath id="master_svg0_134_08844">
+            <rect x="0" y="0" width="16" height="16" rx="0"/>
+          </clipPath>
+        </defs>
+        <g clip-path="url(#master_svg0_134_08844)">
+          <path
+              d="M8.000000047683717,13.199999904632568L3.6000000476837157,8.799999904632568L2.700000047683716,9.699999904632568L7.100000047683716,14.099999904632568L8.000000047683717,14.999999904632569L16.500000047683717,6.499999904632569L15.600000047683716,5.599999904632568L8.000000047683717,13.199999904632568Z"
+              fill="#fff" fill-opacity="1"/>
+        </g>
+        <g>
+          <path
+              d="M15.600000047683716,5.599999904632568L8.000000047683717,13.199999904632568L3.6000000476837157,8.799999904632568L2.700000047683716,9.699999904632568L7.100000047683716,14.099999904632568L8.000000047683717,14.999999904632569L16.500000047683717,6.499999904632569L15.600000047683716,5.599999904632568Z"
+              fill="#fff" fill-opacity="1"/>
+        </g>
+      </svg>
+    </div>
+  </div>
+
+</template>
+<script setup lang="ts">
+
+
+import type {Color} from "@/composables/types";
+import {ref} from "vue";
+
+const model = ref(false)
+function checkFunction() {
+  if (!props.disabled) {
+    model.value = !model.value
+  }
+}
+
+const props = withDefaults(
+    defineProps<{
+      color: Color
+      disabled: boolean
+    }>()
+    , {
+      disable: false,
+      color: "azure"
+    }
+)
+</script>
+<style scoped lang="scss">
+
+</style>
---
+++ vue-sandbox/src/components/NukLoading.vue
@@ -0,0 +1,154 @@
+<template>
+  <div>
+    <div class="lds-grid group">
+      <div
+          class="nuk-coloring bg-[--nuk-primary-60]  group-hover:bg-[--nuk-primary-40]
+           nuk-transition-colors
+           group-hover:ease-in-out dark:bg-[--nuk-primary-50] dark:group-hover:bg-[--nuk-primary-40]"
+          :nuk-color-primary="color"></div>
+      <div
+          class="nuk-coloring bg-[--nuk-primary-60]  group-hover:bg-[--nuk-primary-40]
+           nuk-transition-colors
+           group-hover:ease-in-out dark:bg-[--nuk-primary-50] dark:group-hover:bg-[--nuk-primary-40]"
+          :nuk-color-primary="color"></div>
+      <div
+          class="nuk-coloring bg-[--nuk-primary-60]  group-hover:bg-[--nuk-primary-40]
+           nuk-transition-colors
+           group-hover:ease-in-out dark:bg-[--nuk-primary-50] dark:group-hover:bg-[--nuk-primary-40]"
+          :nuk-color-primary="color"></div>
+      <div
+          class="nuk-coloring bg-[--nuk-primary-60]  group-hover:bg-[--nuk-primary-40]
+           nuk-transition-colors
+           group-hover:ease-in-out dark:bg-[--nuk-primary-50] dark:group-hover:bg-[--nuk-primary-40]"
+          :nuk-color-primary="color"></div>
+      <div
+          class="nuk-coloring bg-[--nuk-primary-60]  group-hover:bg-[--nuk-primary-40]
+           nuk-transition-colors
+           group-hover:ease-in-out dark:bg-[--nuk-primary-50] dark:group-hover:bg-[--nuk-primary-40]"
+          :nuk-color-primary="color"></div>
+      <div
+          class="nuk-coloring bg-[--nuk-primary-60]  group-hover:bg-[--nuk-primary-40]
+           nuk-transition-colors
+           group-hover:ease-in-out dark:bg-[--nuk-primary-50] dark:group-hover:bg-[--nuk-primary-40]"
+          :nuk-color-primary="color"></div>
+      <div
+          class="nuk-coloring bg-[--nuk-primary-60]  group-hover:bg-[--nuk-primary-40]
+           nuk-transition-colors
+           group-hover:ease-in-out dark:bg-[--nuk-primary-50] dark:group-hover:bg-[--nuk-primary-40]"
+          :nuk-color-primary="color"></div>
+      <div
+          class="nuk-coloring bg-[--nuk-primary-60]  group-hover:bg-[--nuk-primary-40]
+           nuk-transition-colors
+           group-hover:ease-in-out dark:bg-[--nuk-primary-50] dark:group-hover:bg-[--nuk-primary-40]"
+          :nuk-color-primary="color"></div>
+      <div
+          class="nuk-coloring bg-[--nuk-primary-60]  group-hover:bg-[--nuk-primary-40]
+           nuk-transition-colors
+           group-hover:ease-in-out dark:bg-[--nuk-primary-50] dark:group-hover:bg-[--nuk-primary-40]"
+          :nuk-color-primary="color"></div>
+    </div>
+  </div>
+</template>
+<script setup lang="ts">
+
+
+
+import type {Color} from "@/composables/types";
+
+const props = withDefaults(
+    defineProps<{
+      color: Color,
+
+    }>(),
+    {
+      color: "azure"
+    }
+)
+
+</script>
+<style scoped>
+
+.lds-grid,
+.lds-grid div {
+  box-sizing: border-box;
+}
+
+.lds-grid {
+  display: inline-block;
+  position: relative;
+  width: 20px;
+  height: 20px;
+}
+
+.lds-grid div {
+  position: absolute;
+  width: 4px;
+  height: 4px;
+  animation: lds-grid 1.2s linear infinite;
+}
+
+.lds-grid div:nth-child(1) {
+  top: 2px;
+  left: 2px;
+  animation-delay: 0s;
+}
+
+.lds-grid div:nth-child(2) {
+  top: 2px;
+  left: 8px;
+  animation-delay: -0.4s;
+}
+
+.lds-grid div:nth-child(3) {
+  top: 2px;
+  left: 14px;
+  animation-delay: -0.8s;
+}
+
+.lds-grid div:nth-child(4) {
+  top: 8px;
+  left: 2px;
+  animation-delay: -0.4s;
+}
+
+.lds-grid div:nth-child(5) {
+  top: 8px;
+  left: 8px;
+  animation-delay: -0.8s;
+}
+
+.lds-grid div:nth-child(6) {
+  top: 8px;
+  left: 14px;
+  animation-delay: -1.2s;
+}
+
+.lds-grid div:nth-child(7) {
+  top: 14px;
+  left: 2px;
+  animation-delay: -0.8s;
+}
+
+.lds-grid div:nth-child(8) {
+  top: 14px;
+  left: 8px;
+  animation-delay: -1.2s;
+}
+
+.lds-grid div:nth-child(9) {
+  top: 14px;
+  left: 14px;
+  animation-delay: -1.6s;
+}
+
+@keyframes lds-grid {
+  0%, 100% {
+    opacity: 1;
+  }
+  50% {
+    opacity: 0.75;
+  }
+}
+
+
+</style>
