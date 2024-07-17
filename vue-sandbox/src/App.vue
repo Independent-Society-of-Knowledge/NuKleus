a5d6a6dfbb4c466ec7e37cdd2abf904a7bd54ace
Kid A <me@thisismeamir.com>
2024-07-17 17:49:56 +0330

    [implementation] NukInput Implemented

---
+++ vue-sandbox/src/App.vue
@@ -0,0 +1,84 @@
+
+<template>
+  <div class="flex flex-row">
+    <nuk-button size="small" :color="computedColor" @click="sizeRef < 6 ? sizeRef += 1 : sizeRef = 1">
+      change size
+    </nuk-button>
+    <nuk-button size="small" :color="computedColor" @click="colorRef < 5 ? colorRef += 1: colorRef = 1">
+      change color
+    </nuk-button>
+    <nuk-button size="small" :color="computedColor" @click="value < 100 ? value += 10: value= 0">
+      change value
+    </nuk-button>
+    <nuk-button size="small" :color="computedColor" @click="()=>{
+      darkMode = !darkMode
+      console.log(darkMode)
+    }">
+      Toggle Dark/Light
+    </nuk-button>
+  </div>
+  <div class="bg-white dark:bg-black flex flex-col justify-center items-center w-screen h-screen p-[100px]" :class="{ dark: darkMode }">
+    <nuk-input :color="computedColor"/>
+  </div>
+</template>
+<script setup lang="ts">
+import NukButton from "@/components/NukButton.vue";
+import {computed, ref} from "vue";
+
+import NukBreadcrumb from "@/components/NukBreadcrumb.vue";
+import NukLoading from "@/components/NukLoading.vue";
+import NukInput from "@/components/NukInput.vue";
+const colorRef = ref(1)
+
+
+const value = ref(0)
+
+const model = ref("1")
+const computedColor = computed(() => {
+  if (colorRef.value === 1) {
+    console.log('azure')
+    return 'azure'
+  } else if (colorRef.value === 2) {
+    console.log('shore')
+    return 'shore'
+  } else if (colorRef.value === 3) {
+    console.log('ocean')
+    return 'ocean'
+  } else if (colorRef.value === 4) {
+    console.log('gray')
+    return 'gray'
+  }
+})
+
+const sizeRef = ref(1)
+const darkMode = ref(false)
+const computedSize = computed(() => {
+  if (sizeRef.value === 1) {
+    console.log('sizeRef', sizeRef.value)
+    console.log('small')
+    return 'small'
+  } else if (sizeRef.value === 2) {
+    console.log('sizeRef', sizeRef.value)
+    console.log('medium')
+    return 'medium'
+  } else if (sizeRef.value === 3) {
+    console.log('sizeRef', sizeRef.value)
+    console.log('large')
+    return 'large'
+  } else if (sizeRef.value === 4) {
+    console.log('sizeRef', sizeRef.value)
+    console.log('xlarge')
+    return 'xlarge'
+  } else if (sizeRef.value === 5) {
+    console.log('sizeRef', sizeRef.value)
+    console.log('max')
+    return 'max'
+  }
+})
+</script>
+
+<style scoped>
+*{
+  @apply selection:bg-[--nuk-primary-70];
+}
+</style>
---
+++ vue-sandbox/src/components/NukInput.vue
@@ -0,0 +1,60 @@
+
+<template>
+  <input
+      class="
+      nuk-coloring
+      nuk-text-body-01
+      nuk-transition-all-fast-02-standard-productive
+      px-16px
+      py-8px
+      outline-none
+      border-solid
+      border-b-[1px]
+      border-[--nuk-primary-70]
+      border-opacity-0
+      active:border-b-[4px]
+      focus:border-b-[4px]
+
+
+      bg-[--nuk-secondary-5]
+      dark:bg-[--nuk-secondary-80]
+
+      dark:placeholder:text-[--nuk-secondary-60]
+      placeholder:text-[--nuk-secondary-30]
+      placeholder:transition-colors
+      placeholder:hover:text-[--nuk-secondary-80]
+      dark:placeholder:hover:text-[--nuk-secondary-50]
+      text-black
+      dark:text-white
+
+
+
+
+"
+      :nuk-color-primary="color"
+      nuk-color-secondary="gray"
+      :type="type" :placeholder="placeholder"/>
+</template>
+<script setup lang="ts">
+
+import type {Color} from "@/composables/types";
+
+const props = withDefaults(
+    defineProps<{
+      color: Color
+      type: string
+      placeholder: string
+    }>(),
+    {
+      color: "azure",
+      type: "text",
+      placeholder: "Placeholder"
+    }
+)
+
+
+</script>
+
+<style scoped>
+
+</style>
