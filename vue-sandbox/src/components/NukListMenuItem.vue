5ce962f91f72b9c1dfeb41b4480b03b5a4139811
Kid A <me@thisismeamir.com>
2024-07-17 18:53:03 +0330

    [Implementation] NukListMenuItem Implemented

---
+++ vue-sandbox/src/components/NukListMenuItem.vue
@@ -0,0 +1,79 @@
+
+
+<template>
+  <div class="
+  group
+  nuk-coloring
+  flex flex-row
+  items-center
+  nuk-transition-all-fast-02-standard-productive
+  w-full
+  bg-[--nuk-secondary-10]
+  dark:bg-[--nuk-secondary-80]
+  [&[nuk-type-handler='full-color']]:hover:bg-[--nuk-primary-70]
+  flex-start
+
+  border-solid
+  border-l-[2px]
+  hover:border-l-[4px]
+  border-l-[--nuk-secondary-60]
+  hover:border-l-[--nuk-primary-70]
+  p-16px
+  dark:text-white
+  text-black
+  nuk-text-body-01
+  [&[nuk-color-primary='gray]]:text-black
+  dark:[&[nuk-color-primary='gray']]:text-white
+
+  disabled:dark:bg-[--nuk-secondary-80]
+  disabled:bg-[--nuk-secondary-10]
+  disabled:dark:text-[--nuk-secondary-70]
+  disabled:text-[--nuk-secondary-30]
+
+  gap-4px
+  hover:gap-[16px]
+  active:gap-[16px]
+
+"
+       :nuk-type-handler="type"
+       :nuk-color-primary="color"
+       nuk-color-secondary="gray"
+  >
+
+    <slot name="icon" >
+      <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" fill="none" version="1.1" width="16"
+           height="16" viewBox="0 0 32 32">
+        <defs>
+          <clipPath id="master_svg0_134_09036">
+            <rect x="0" y="0" width="32" height="32" rx="0"/>
+          </clipPath>
+        </defs>
+        <g clip-path="url(#master_svg0_134_09036)">
+          <g>
+            <path d="M17,15L17,8L15,8L15,15L8,15L8,17L15,17L15,24L17,24L17,17L24,17L24,15L17,15Z" fill="currentColor"
+                  fill-opacity="1"/>
+          </g>
+          <g></g>
+        </g>
+      </svg>
+    </slot>
+    <slot name="default"/>
+  </div>
+</template>
+<script setup lang="ts">
+import type {Color} from "@/composables/types";
+
+const props = withDefaults(
+    defineProps<{
+      color: Color
+      type: 'full-color' | 'bordered'
+    }>(),
+    {
+      color: 'azure',
+      type: 'bordered'
+    }
+)
+</script>
+<style scoped>
+
+</style>
