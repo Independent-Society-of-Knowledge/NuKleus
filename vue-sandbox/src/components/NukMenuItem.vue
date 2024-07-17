54b473a6b8b033930ef2165d234b00827c8af22a
Kid A <me@thisismeamir.com>
2024-07-17 17:50:10 +0330

    [implementation] NukInput Implemented

---
+++ vue-sandbox/src/components/NukMenuItem.vue
@@ -0,0 +1,25 @@
+
+<template>
+  <div class="
+  nuk-coloring
+
+"
+       :nuk-color-primary="color"
+  >
+    <slot name="icon"/>
+    <slot name="default"/>
+  </div>
+</template>
+<script setup lang="ts">
+import type {Color} from "@/composables/types";
+
+const props = withDefaults(
+    defineProps<{
+      color: Color
+    }>(),
+    {}
+)
+</script>
+<style scoped>
+
+</style>
